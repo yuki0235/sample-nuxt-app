e8725d92f9d7dd0077f86b6a20c576338bb2b30b
Nyakan1sh1 <nyakanishi.harry@gmail.com>
2022-03-11 20:54:27 +0900

    Add users page and validate id

---
+++ pages/users/_id.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    this is user: {{ this.$route.params.id }} page
+  </div>
+</template>
+
+<script>
+  export default ({
+    validate({ params }) {
+      return /^\d+$/.test(params.id)
+    }
+  })
+</script>
