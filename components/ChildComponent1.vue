891ed345b1e42e1e215da7699e9cad4f3fdebebc
Nyakan1sh1 <nyakanishi.harry@gmail.com>
2022-03-12 20:47:14 +0900

    lesson for component and props(with v-mode,v-bind)

---
+++ components/ChildComponent1.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <v-btn>{{ message }}</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'message',
+  ]
+}
+</script>
+
+<!--scopedを付与すると、ここで定義したCSSはこのコンポーネントのみに適用される-->
+<style scoped>
+
+</style>
---
+++ pages/parent_component1.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!--    v-modelを使用してsampleTextプロパティと紐づける-->
+    <!--    テキストフィールドに入力した値が動的に変更される-->
+    <v-text-field label="please typing..." v-model="sampleText"></v-text-field>
+
+    <!--    コンポーネントにsampleTextの値をpropsとして渡す-->
+    <!--    この場合はv-bindを指定してあげる必要がある-->
+    <ChildComponent1 v-bind:message="sampleText" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sampleText: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
