ea0351d20fa329ce1416e70a9c281aa8f3388f69
Nyakan1sh1 <nyakanishi.harry@gmail.com>
2022-03-13 14:57:52 +0900

    add record button

--- pages/time_management.vue
+++ pages/time_management.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="container">
+    <!--    現在時刻表示（毎秒更新）-->
     <v-card elevation="2" outlined>
       <h1>現在の日時： {{ date }} {{ time }}</h1>
     </v-card>
+
+    <!--    記録用ボタン-->
+    <v-btn @click="updateRecord" elevation="2" large :color="color" class="recordButton">
+      <span v-if="recordFlag">記録開始</span>
+      <span v-else>記録終了</span>
+    </v-btn>
   </div>
 </template>
 
@@ -12,7 +19,10 @@ export default {
   data () {
     return {
       date: '',
-      time: ''
+      time: '',
+      recordFlag: true,
+      buttonMessage: false,
+      color: 'primary',
     }
   },
   mounted: function() {
@@ -23,6 +33,14 @@ export default {
     updateDateTime() {
       this.date = moment().format('YYYY/MM/DD')
       this.time = moment().format('HH:mm:ss')
+    },
+    updateRecord() {
+      this.recordFlag = !this.recordFlag
+      if (this.recordFlag) {
+        this.color = 'primary'
+      } else {
+        this.color = 'secondary'
+      }
     }
   }
 }
@@ -32,5 +50,9 @@ export default {
   h1 {
     text-align: center;
     padding: 2em 0;
+  }
+  .recordButton {
+    margin-top: 2em;
+    width: 100%;
   }
 </style>
